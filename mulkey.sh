34d221cecfcc86c24282c75aaed0bf906bd7482f
Gaveen Prabhasara <gaveen@owain.org>
2016-07-14 20:52:29 +0530

    Update: mulkey.sh

--- mulkey.sh
+++ mulkey.sh
@@ -2,13 +2,20 @@
 #
 # mulkey - a simple wrapper for media player functions (my_desk)
 #
-#       PLAYER      - player command name
-#       ACT_PREV    - player action: previous
-#       ACT_NEXT    - player action: next
-#       ACT_TOGL    - player action: toggle pause/resume
+# To define a new player use the following syntax:
+#       PLAYER_PREF[<id>]="<program> <--previous> <--toggle> <--next>"
+#
+#       Where:
+#           <id>            = ID to refer the player
+#           <program>       = Command to issue player actions
+#           <--previous>    = <program> Option to play previous track
+#           <--toggle>      = <program> Option to play/pause toggle
+#           <--next>        = <program> Option to play next track
+#
+#       e.g.,:
+#       PLAYER_PREF[1]="mocp --previous --toggle-pause --next"
 #
 # Useful when binding actions to multimedia keys
-# Inspired by: https://gist.github.com/chanux/db01bd2c66effc7a259f
 #
 
 ARGS=1
@@ -16,34 +23,44 @@ E_BADARGS=65
 E_NOFILE=66
 E_NOPROG=67
 
-PLAYER="mocp"
-ACT_PREV="--previous"
-ACT_NEXT="--next"
-ACT_TOGL="--toggle-pause"
-
 if [ ! $# == $ARGS ] ; then
     echo "Usage: `basename $0`  [ --prev | --toggle | --next ]"
     exit $E_BADARGS
 fi
 
-if pgrep -c $PLAYER
-then
-    case $1 in
-        --next)
-            $PLAYER $ACT_NEXT
-            ;;
-        --prev)
-            $PLAYER $ACT_PREV
-            ;;
-        --toggle)
-            $PLAYER $ACT_TOGL
-            ;;
-        *)
-            echo "Usage: `basename $0`  [ --prev | --toggle | --next ]"
-            exit $E_BADARGS
-            ;;
-    esac
-else
-    echo "Error: $PLAYER is not running"
-    exit $E_NOPROG
-fi
+# Define players and actions
+declare -A PLAYER_PREF
+PLAYER_PREF[0]="audacious --rew --play-pause --fwd"
+PLAYER_PREF[1]="mocp --previous --toggle-pause --next"
+PLAYER_PREF[2]="rhythmbox-client --previous --play-pause --next"
+
+# Do the action only for specified players
+for i in 0 1 2
+do
+    PLAYER=$(echo ${PLAYER_PREF[$i]} | cut -f1 -d " ")
+    ACT_PREV=$(echo ${PLAYER_PREF[$i]} | cut -f2 -d " ")
+    ACT_TOGL=$(echo ${PLAYER_PREF[$i]} | cut -f3 -d " ")
+    ACT_NEXT=$(echo ${PLAYER_PREF[$i]} | cut -f4 -d " ")
+
+    if pgrep -c $PLAYER
+    then
+        case $1 in
+            --prev)
+                $PLAYER $ACT_PREV
+                ;;
+            --togl)
+                $PLAYER $ACT_TOGL
+                ;;
+            --next)
+                $PLAYER $ACT_NEXT
+                ;;
+            *)
+                echo "Usage: `basename $0`  [ --prev | --togl | --next ]"
+                exit $E_BADARGS
+                ;;
+        esac
+    else
+        echo "Error: $PLAYER is not running"
+        exit $E_NOPROG
+    fi
+done
