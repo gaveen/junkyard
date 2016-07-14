52acc4c2a98028ab27d81eadb466bbb77e5b125f
Gaveen Prabhasara <gaveen@owain.org>
2016-07-14 21:11:27 +0530

    Update: mulkey.sh

--- mulkey.sh
+++ mulkey.sh
@@ -2,8 +2,8 @@
 #
 # mulkey - a simple wrapper for media player functions (my_desk)
 #
-# To define a new player use the following syntax:
-#       PLAYER_PREF[<id>]="<program> <--previous> <--toggle> <--next>"
+# To define a new player use the following syntax (space delimited):
+#       PLAYER_DEF[<id>]="<program> <--previous> <--toggle> <--next>"
 #
 #       Where:
 #           <id>            = ID to refer the player
@@ -13,7 +13,7 @@
 #           <--next>        = <program> Option to play next track
 #
 #       e.g.,:
-#       PLAYER_PREF[1]="mocp --previous --toggle-pause --next"
+#       PLAYER_DEF[1]="mocp --previous --toggle-pause --next"
 #
 # Useful when binding actions to multimedia keys
 #
@@ -24,35 +24,35 @@ E_NOFILE=66
 E_NOPROG=67
 
 if [ ! $# == $ARGS ] ; then
-    echo "Usage: `basename $0`  [ --prev | --toggle | --next ]"
+    echo "Usage: `basename $0`  [ --prev | --togl | --next ]"
     exit $E_BADARGS
 fi
 
 # Define players and actions
-declare -A PLAYER_PREF
-PLAYER_PREF[0]="audacious --rew --play-pause --fwd"
-PLAYER_PREF[1]="mocp --previous --toggle-pause --next"
-PLAYER_PREF[2]="rhythmbox-client --previous --play-pause --next"
+declare -A PLAYER_DEF
+PLAYER_DEF[audacious]="audacious --rew --play-pause --fwd"
+PLAYER_DEF[moc]="mocp --previous --toggle-pause --next"
+PLAYER_DEF[rhythmbox]="rhythmbox-client --previous --play-pause --next"
 
 # Do the action only for specified players
-for i in 0 1 2
+for i in audacious moc rhythmbox
 do
-    PLAYER=$(echo ${PLAYER_PREF[$i]} | cut -f1 -d " ")
-    ACT_PREV=$(echo ${PLAYER_PREF[$i]} | cut -f2 -d " ")
-    ACT_TOGL=$(echo ${PLAYER_PREF[$i]} | cut -f3 -d " ")
-    ACT_NEXT=$(echo ${PLAYER_PREF[$i]} | cut -f4 -d " ")
+    PLAY_CLI=$(echo ${PLAYER_DEF[$i]} | cut -f1 -d " ")
+    ACT_PREV=$(echo ${PLAYER_DEF[$i]} | cut -f2 -d " ")
+    ACT_TOGL=$(echo ${PLAYER_DEF[$i]} | cut -f3 -d " ")
+    ACT_NEXT=$(echo ${PLAYER_DEF[$i]} | cut -f4 -d " ")
 
-    if pgrep -c $PLAYER
+    if pgrep -c $PLAY_CLI
     then
         case $1 in
             --prev)
-                $PLAYER $ACT_PREV
+                $PLAY_CLI $ACT_PREV
                 ;;
             --togl)
-                $PLAYER $ACT_TOGL
+                $PLAY_CLI $ACT_TOGL
                 ;;
             --next)
-                $PLAYER $ACT_NEXT
+                $PLAY_CLI $ACT_NEXT
                 ;;
             *)
                 echo "Usage: `basename $0`  [ --prev | --togl | --next ]"
@@ -60,7 +60,7 @@ do
                 ;;
         esac
     else
-        echo "Error: $PLAYER is not running"
+        echo "Error: $PLAY_CLI is not running"
         exit $E_NOPROG
     fi
 done
